0b65a1a3a48ec89f2d220f8af7f5b9fd4d86a572
robert-chheav <robert.chheav@gmail.com>
2024-01-03 15:50:02 +0100

    update layout on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import picture from "../assets/profile-pic.png";
+import Blob from "./Blob";
 const Header = () => {
   return (
-    <>
-      <span>Hey there! I'm </span>
-      <h1 className="text-3xl font-extrabold">Robert Chheav</h1>
-      <span>Software Engineer</span>
-      <p>Experienced in Customer experiences and business</p>
-      <img src={picture} className="max-w-52 md:max-w-80" alt="robert profile picture" />
-    </>
+    <header className="relative mt-10 sm:mx-24">
+      <Blob />
+      <div className="grid grid-cols-3">
+        <div className="sm:col-span-2">
+          <span className="text-xl font-extrabold">Hey there! I'm </span>
+          <h1 className="text-3xl font-extrabold bg-gradient-to-r from-red-500 to-blue-500 text-transparent bg-clip-text table">
+            Robert Chheav
+          </h1>
+          <span className="text-xl font-bold">Software Engineer</span>
+          <p>Experienced in Customer experiences and business</p>
+        </div>
+        <img
+          src={picture}
+          className="max-w-52 md:max-w-80"
+          alt="robert profile picture"
+        />
+      </div>
+    </header>
   );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,23 @@
 import React from "react";
 import { navLinks } from "../assets";
-import logo from "../assets/logo.svg"
+import logo from "../assets/logo.svg";
 // import { logo } from "../assets";
 const Navbar = () => {
   return (
-    <>
-      <nav className="flex justify-between items-center">
-        <a href="/index.html" className="flex w-10">
-          <img src={logo} alt="" />
-        </a>
-        <ul className="flex gap-5">
-          <li className="hover:text-cyan-400">
-            <a href="#skills">Skills</a>
-          </li>
-          <li>
-            <a href="#project">Project</a>
-          </li>
-          <li>About me</li>
-        </ul>
-      </nav>
-    </>
+    <nav className="flex justify-between items-center sm:mx-20">
+      <a href="/index.html" className="flex w-10">
+        <img src={logo} alt="" />
+      </a>
+      <ul className="flex gap-5">
+        <li className="hover:text-cyan-400">
+          <a href="#skills">Skills</a>
+        </li>
+        <li>
+          <a href="#project">Project</a>
+        </li>
+        <li>About me</li>
+      </ul>
+    </nav>
   );
 };
 
