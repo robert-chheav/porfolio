ed4071684ab0325bcde15f73daaee826c6571bd2
robert-chheav <robert.chheav@gmail.com>
2024-01-08 13:24:05 +0100

    grid update

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,10 +2,15 @@ import React from "react";
 
 const Skills = () => {
   return (
-    <section className="relative mt-10 sm:mx-24">
-      <div>
-        <h3>Hard skills</h3>
+    <section className="relative mt-10 sm:mx-24 grid grid-cols-2">
+      <div className="col-span-1 col-start-1 col-end-2 row-span-2">
+        <h3>Soft skills</h3>
         <ul>
+          <li>1</li>
+          <li>1</li>
+          <li>1</li>
+          <li>1</li>
+          <li>1</li>
           <li>1</li>
           <li>1</li>
           <li>1</li>
@@ -14,16 +19,18 @@ const Skills = () => {
           <li>1</li>
         </ul>
       </div>
-      <h3>Hard skills</h3>
-      <ul>
-        <li>2</li>
-        <li>2</li>
-        <li>2</li>
-        <li>2</li>
-        <li>2</li>
-        <li>2</li>
-      </ul>
-      <div>
+      <div className="col-span-1 row-span-1">
+        <h3>Soft skills</h3>
+        <ul>
+          <li>2</li>
+          <li>2</li>
+          <li>2</li>
+          <li>2</li>
+          <li>2</li>
+          <li>2</li>
+        </ul>
+      </div>
+      <div className="col-span-1 col-start-2 col-end-3 row-span-1">
         <h3>Languages</h3>
         <ul>
           <li>French</li>
