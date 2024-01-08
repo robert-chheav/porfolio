fcaaa32ab0189ac6c3f927a65bb562e1f27375c7
robert-chheav <robert.chheav@gmail.com>
2024-01-08 13:07:01 +0100

    skill architecture created

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
 
 const Skills = () => {
   return (
-    <section>Skills</section>
-  )
-}
+    <section className="relative mt-10 sm:mx-24">
+      <div>
+        <h3>Hard skills</h3>
+        <ul>
+          <li>1</li>
+          <li>1</li>
+          <li>1</li>
+          <li>1</li>
+          <li>1</li>
+          <li>1</li>
+        </ul>
+      </div>
+      <h3>Hard skills</h3>
+      <ul>
+        <li>2</li>
+        <li>2</li>
+        <li>2</li>
+        <li>2</li>
+        <li>2</li>
+        <li>2</li>
+      </ul>
+      <div>
+        <h3>Languages</h3>
+        <ul>
+          <li>French</li>
+          <li>English</li>
+          <li></li>Chinese
+        </ul>
+      </div>
+    </section>
+  );
+};
 
-export default Skills
+export default Skills;
