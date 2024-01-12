461c0469acd81c4450d8b4f8612f79c2a441c64b
robert-chheav <robert.chheav@gmail.com>
2024-01-12 10:47:29 +0100

    footer component created

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar'
 import Blob from './components/Blob'
 import Header from './components/Header'
 import Skills from './components/Skills'
+import Footer from './components/Footer'
 function App() {
 
   return (
@@ -13,6 +14,7 @@ function App() {
       <Blob />
       <Header/>
       <Skills/>
+      <Footer/>
     </div>
     </>
   )
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="flex justify-center w-dvw mt-10 h-44 ">
+
+      <div className=""></div>
+      Designed and built by Robert Chheav | 2024
+    </footer>
+  );
+};
+
+export default Footer;
