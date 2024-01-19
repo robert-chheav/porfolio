112be046bf5a4807ba470fa673c4859ae6438030
robert-chheav <robert.chheav@gmail.com>
2024-01-19 14:40:53 +0100

    h3 change

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,7 +3,7 @@ import PetApp from "../assets/home-screen-app.png";
 const Projects = () => {
   return (
     <section>
-      <h3 className="text-xl sm:text-3xl font-bold mb-3 mt-5 sm:mt-0">
+      <h3 className="flex text-xl sm:text-3xl font-bold mb-3 mt-5 sm:mt-0">
         Projects
       </h3>
 
